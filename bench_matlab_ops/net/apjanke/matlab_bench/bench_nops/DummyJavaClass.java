a87f8738546c92c727e120aef61612c4d337a482
Andrew Janke <janke@pobox.com>
2014-05-29 16:17:06 -0400

    Recompile the Java classes with JDK 1.6 for back-compatibility. Add a main() for easy testing.

--- bench_matlab_ops/net/apjanke/matlab_bench/bench_nops/DummyJavaClass.java
+++ bench_matlab_ops/net/apjanke/matlab_bench/bench_nops/DummyJavaClass.java
@@ -19,4 +19,13 @@ class DummyJavaClass {
 	        nop();
 	    }
 	}
+
+	/**
+	 * Main method just prints "Hello, world!". This is not
+	 * used for anything; it's just here to make it easy to test
+	 * loading this class in various JVMs.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
 }
