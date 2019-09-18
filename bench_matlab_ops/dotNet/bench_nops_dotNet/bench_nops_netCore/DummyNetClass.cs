4cf8010d580a91b0a3a2f7ccae8ab293cb4ca388
Kappen, Jan <Jan.Kappen@te.com>
2019-09-18 14:45:45 +0200

    Add test cases for C# .NET Fw 4.5 and .NET Standard 2.0 similar to java tests

---
+++ bench_matlab_ops/dotNet/bench_nops_dotNet/bench_nops_netCore/DummyNetClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace bench_nops_netCore
+{
+    public class DummyNetClass
+    {
+        public void nop() { }
+        public static void staticNop() { }
+        public void callNop(int nIters)
+        {
+            for (int i = 0; i < nIters; i++)
+            {
+                nop();
+            }
+        }
+
+    }
+}
---
+++ bench_matlab_ops/dotNet/bench_nops_dotNet/bench_nops_netFw45/DummyNetClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bench_nops_netFw45
+{
+    public class DummyNetClass
+    {
+        public void nop() { }
+        public static void staticNop() { }
+        public void callNop(int nIters)
+        {
+            for (int i = 0; i < nIters; i++)
+            {
+                nop();
+            }
+        }
+
+    }
+}
